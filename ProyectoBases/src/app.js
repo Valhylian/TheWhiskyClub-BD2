35a941160b0505ebf9be43b15db463277d5d3d31
Valhylian <valeria44a@estudiantec.cr>
2022-06-16 12:13:58 -0600

    Email features ready

--- ProyectoBases/src/app.js
+++ ProyectoBases/src/app.js
@@ -18,8 +18,45 @@ import multer from "multer";
 
 const fs = require('fs')
 
+//MAIL
+const nodemailer = require('nodemailer');
+
 const app = express();
 import('./config/passport');
+
+//MAIL
+let transporter = nodemailer.createTransport({
+    host: "smtp-mail.outlook.com", // hostname
+    secureConnection: false, // TLS requires secureConnection to be false
+    port: 587, // port for secure SMTP
+    tls: {
+       ciphers:'SSLv3'
+    },
+    auth: {
+        user: 'shopatwhiskybrothers@outlook.com',
+        pass: 'proyectobases2'
+    }
+});
+
+var mailOptions = {
+    from: 'shopatwhiskybrothers@outlook.com', // sender address (who sends)
+    to: 'valeria44a@gmail.com', // list of receivers (who receives)
+    subject: 'Hello ', // Subject line
+    text: 'Hello world ', // plaintext body
+    html: '<b>Hello world </b><br> This is the first email sent with Nodemailer in Node.js' // html body
+};
+
+/*transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        return console.log(error);
+    }
+
+    console.log('Message sent: ' + info.response);
+});*/
+
+
+
+
 
 // settings
 app.set('port', config.port);
--- ProyectoBases/src/controllers/purchase.controller.js
+++ ProyectoBases/src/controllers/purchase.controller.js
@@ -25,6 +25,22 @@ export const getProducts_store = async (req, res) => {
 
 };
 
+const nodemailer = require('nodemailer');
+
+//TRANSPORTER FOR MAILS------------------------------------------------
+let transporter = nodemailer.createTransport({
+  host: "smtp-mail.outlook.com", // hostname
+  secureConnection: false, // TLS requires secureConnection to be false
+  port: 587, // port for secure SMTP
+  tls: {
+     ciphers:'SSLv3'
+  },
+  auth: {
+      user: 'shopatwhiskybrothers@outlook.com',
+      pass: 'proyectobases2'
+  }
+});
+//--------------------------------------------------------------------------
 export const completePurchase_store = async (req, res) => {
   console.log(req.body.idStoreBought);
   console.log("hola");
@@ -61,6 +77,65 @@ export const completePurchase_store = async (req, res) => {
       .execute(`returnPurchase`);
     const purchaseInfo = resultTypes.recordset;
     
+    //SEN MAIL------------------------------------------------------------------------
+ 
+
+    //BODY EMAIL
+    const clientName = purchaseInfo[0].name_client
+    const clientMail = purchaseInfo[0].email_client
+
+    const purchaseCode = purchaseInfo[0].id_purchase
+    const purchaseLocation = purchaseInfo[0].direction
+    const purchaseShipping = purchaseInfo[0].shipping
+    const total = purchaseInfo[0].total
+    const subtotal = purchaseInfo[0].subtotal
+    const moneyType = purchaseInfo[0].moneyType
+
+    let client_sub = 'NA'
+    let store = 'NA'
+    if (productsResult.length > 0){
+      client_sub = productsResult[0].client_sub
+      store = productsResult[0].store_name
+    }
+
+    let body = '<b> Dear '+clientName+'</b><br> Confirmation of your purchase: '+purchaseCode+' is attached. </b><br>'
+    body = body + 'Direction: '+purchaseLocation + '</b><br>'
+    body = body + 'Store: '+store + '</b><br>'
+    body = body + 'The benefits of your subscription were applied: '+client_sub + '</b><br>'
+    body = body + 'Money Type: '+moneyType + '</b><br>'
+    body = body + 'Shipping Price: '+purchaseShipping + '</b><br>'
+    body = body + 'subtotal: '+subtotal + '</b><br>'
+    body = body + 'Total: '+total + '</b><br>'
+    body = body + 'Status: '+purchaseInfo[0].name_StatusPurchase + '</b><br>'
+
+    for(var k in productsResult) {
+      body = body + '</b><br><pre>Product: '+ productsResult[k].product_name+'</pre>' 
+      body = body + 'Price: '+ productsResult[k].product_priceInd 
+      body = body + '</b><br>Quantity: '+ productsResult[k].product_amount 
+      body = body + '</b><br>Subtotal: '+ productsResult[k].product_subtotal
+      body = body + '</b><br>Status: '+ productsResult[k].product_success 
+   }
+
+    body = body + '</b><br></b><br>Your purchase has not been sent, when it is processed you will receive another email with the necessary information.'
+    body = body +'</b><br>If the status is canceled it will not be processed'
+
+    var mailOptions = {
+      from: 'shopatwhiskybrothers@outlook.com', // sender address (who sends)
+      to: clientMail, // list of receivers (who receives)
+      subject: 'PURCHASE CONFIRMATION', // Subject line
+      text: 'Dear: '+clientName ,  // plaintext body
+      html:  body// html body
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+          return console.log(error);
+      }
+  
+      console.log('Message sent: ' + info.response);
+    });
+
+
     res.render('purchase/purchaseConfirm', { productsResult,purchaseInfo});
   } catch (error) {
     res.status(500);
