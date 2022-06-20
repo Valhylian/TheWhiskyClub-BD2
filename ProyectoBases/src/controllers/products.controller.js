a034de2cb8a23bab791840a2fda0ecbf42da506b
andres190601 <69825401+andres190601@users.noreply.github.com>
2022-06-20 12:47:05 -0600

    display product image
    product tab now displays an image for each product

--- ProyectoBases/src/controllers/products.controller.js
+++ ProyectoBases/src/controllers/products.controller.js
@@ -12,13 +12,27 @@ export const getAllProducts_NotSpecial = async (req, res) => {
     const resultTypes = await pool.request()
       .execute(`returnProductTypes`);
     const typesResult = resultTypes.recordset;
+    
+    //console.log(imagePr2);
 
-    res.render('products/getProducts', { productsResult, typesResult });
+    productsResult.forEach(element => {
+      if (element.product_image != null){
+        element.product_image = "data:image/gif;base64,"+element.product_image.toString('base64')
+        //console.log(element.product_image);
+        //prueba = element.product_image
+      }
+      
+    });
+    //console.log(prueba);
+    //console.log(imagePr2);
+    res.render('products/getProducts', {productsResult, typesResult });
   } catch (error) {
     res.status(500);
     res.send(error.message);
   }
 };
+
+
 
 export const addProductToCart = (req, res) => {
   const { idProduct, quantity } = req.body;
@@ -47,9 +61,20 @@ export const getAllProducts_search = async (req, res) => {
       .execute(`consultProductsParameters`);
 
     const productsResult = result.recordset;
+
+    productsResult.forEach(element => {
+      if (element.product_image != null){
+        element.product_image = "data:image/gif;base64,"+element.product_image.toString('base64')
+        //console.log(element.product_image);
+        //prueba = element.product_image
+      }
+      
+    });
+    
     const resultTypes = await pool.request()
       .execute(`returnProductTypes`);
     const typesResult = resultTypes.recordset;
+    
     res.render('products/getProductsSearch', { productsResult, typesResult });
   } catch (error) {
     res.status(500);
